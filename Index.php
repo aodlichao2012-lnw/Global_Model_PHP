5b2328eed1ed29af487e6d86b53b6968f31d679d
aodlichao2012-lnw <52318749+aodlichao2012-lnw@users.noreply.github.com>
2023-10-24 17:19:56 +0700

    aaaaa

--- Index.php
+++ Index.php
@@ -1,10 +1,4 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
     <?php
     // require './Exten.php';
     // use ViewModel;
@@ -14,44 +8,45 @@
 
 
 
-    echo"Hello world";
+    // echo"Hello world";
 
     $arr = array("12","333","444444444444","4444");
     $dataform_array_to_json = json_encode($arr);
-    echo $dataform_array_to_json[0];
-    foreach($arr as $item ){
-        echo "<div> arr : $item</div>";
-    }
+    // echo $dataform_array_to_json[0];
+    // foreach($arr as $item ){
+    //     echo "<div> arr : $item</div>";
+    // }
     $newobject = new ViewModel();
     $newobject->age = "age is 49";
     ViewModel::$name = 'Static member';
     $newobject->id = "id is 1";
-
+    $datahash = password_hash( $newobject->age , PASSWORD_DEFAULT );
+    // echo "Datahash :". $datahash;
     $list = [];
     $list[] = $newobject;
-    foreach($list as $item){
+    // foreach($list as $item){
 
-        echo $item->age;
-        echo $item->id;
-        echo ViewModel::$name;
-    }
+    //     echo $item->age;
+    //     echo $item->id;
+    //     echo ViewModel::$name;
+    // }
 
     $Extention = new Exten();
+    $data =  $Extention->SelectAll("PREGOV;CORRGOV;ORAIEC23");
+
     $Extention->Log("This is test log");
     $dataform_array_to_json = json_encode($arr);
     $data_from_json = json_decode($dataform_array_to_json);
-    $Extention->ExportPDF($data_from_json);
+
+    // $Extention->ExportPDF($data,$keys);
+    $Extention->ExportCSV("SELECT ANUMBER , LEAD_CALL_DATE FROM MAS_LEADS_TRANS WHERE ROWNUM <= 1000","PREGOV;CORRGOV;ORAIEC23");
     function Test1(){
         global $arr , $dataform_array_to_json;
         $data_from_json = json_decode($dataform_array_to_json);
         foreach($data_from_json as $item){
-            echo "<div> data_from_json : $item </div>";
+            // echo "<div> data_from_json : $item </div>";
             file_put_contents("D://Test.txt",$item."\n" , FILE_APPEND);
         }
     }
     ?>
-    <div>   
-        <? Test1() ?>
-    </div>
-</body>
-</html>
+
